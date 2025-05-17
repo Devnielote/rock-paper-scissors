cda56fdbdf2533bb36635cb31833c732078302e1
Devnielote <danielfc_26@hotmail.com>
2025-05-17 15:59:04 -0600

    Modify name for consistency

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-class Game
+import { IBoard } from  './core/types.ts'
+
+export class Game {
+  private board: GameBoard 
+  private ruleSet: RuleSet
+  private player1: Player 
+  private player2: Player
+
+  constructor(board: GameBoardm, ruleSet: RuleSet, player1: Player, player2: Player ){
+    this.board =  board,
+    this.ruleSet = ruleSet,
+    this.player1 = player1,
+    this.player2 = player2
+  }
+
+  startGame(): void {
+
+  }
+}
