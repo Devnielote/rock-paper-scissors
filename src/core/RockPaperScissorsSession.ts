9ad5a3fb91a57dfd0e65507177d2ca9e3779c007
Devnielote <danielfc_26@hotmail.com>
2025-05-19 22:29:39 -0600

    refactor: Delete unused types + add properties for controlling rounds and game status + create methods for round checking flow

--- src/core/RockPaperScissorsSession.ts
+++ src/core/RockPaperScissorsSession.ts
@@ -1,8 +1,12 @@
-import { ClassicMode, ClassicPlays, GameSession } from "./interfaces/GameSession";
+import { GameSession } from "./interfaces/GameSession";
+import { BigBangMode, ClassicMode, ClassicPlays } from "./types/types";
 
 export class RockPaperScissorsSession implements GameSession {
   gameMode: ClassicMode = 'classic';
   availablePlays: ClassicPlays[] = [];
+  isGameOver: boolean = false;
+  roundsToWin: number = 0;
+  currentRound: number = 0;
 
   //Valid plays available 
   private modeToPlaysMap: Record<ClassicMode,ClassicPlays[]> = {
@@ -14,8 +18,38 @@ export class RockPaperScissorsSession implements GameSession {
     this.setAvailablePlaysBasedOnGameMode(mode);
   }
 
+  setRoundsToWin(value: number): void {
+     this.roundsToWin = value; 
+  }
+
   setAvailablePlaysBasedOnGameMode(mode: ClassicMode): void {
     this.availablePlays = this.modeToPlaysMap[mode];
   }
 
+  setIsGameOver(): void {
+    !this.isGameOver ? this.isGameOver = true: false;
+  }
+
+  incrementRoundsToWin(): void {
+    this.roundsToWin++
+  }
+
+  getRoundsToWin(): number {
+    return this.roundsToWin;
+  }
+  
+  getCurrentRound(): number {
+    return this.currentRound;
+  }
+
+  checkIfSessionIsOver(): void | null{
+    if(this.getCurrentRound() >= this.getRoundsToWin()){
+      this.setIsGameOver();
+    }
+    else {
+      return null;
+    }
+  }
+
 }
+
