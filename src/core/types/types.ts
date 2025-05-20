86394c22283eaab3f5227eb71febf2d71a0b51e5
Devnielote <danielfc_26@hotmail.com>
2025-05-19 22:21:59 -0600

    refactor: create type file. All types are here

---
+++ src/core/types/types.ts
@@ -0,0 +1,5 @@
+export type ClassicMode = 'classic';
+export type BigBangMode = 'big bang';
+
+export type ClassicPlays = 'rock'|'paper'|'scissors';
+export type BigBangPlays = 'rock'|'paper'|'scissors'|'lizard'|'spock';
