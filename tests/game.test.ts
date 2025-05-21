c5462b5b1e143b2150da134b6a1fbf73f2d61158
Devnielote <danielfc_26@hotmail.com>
2025-05-21 15:37:49 -0600

    Delete unused tests files

--- tests/game.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import { createBoard } from '../src/game'
-
-describe('createBoard'), () => {
-  it('must return a score board and game board'), () => {
-    const board = createBoard()
-    expect(board).toEqual()
-  }
-}
