353e38c30994e4ebd533694d6b23667b2e0fc657
Devnielote <danielfc_26@hotmail.com>
2025-05-23 11:37:31 -0600

    refactor: add getPrompt method to Player interface

--- src/core/interfaces/Player.ts
+++ src/core/interfaces/Player.ts
@@ -2,6 +2,7 @@ import { BigBangPlays, ClassicPlays } from "../types/types";
 
 export interface Player<TPlays = ClassicPlays | BigBangPlays> {
   makePlay(play:TPlays | null):void;
+  getPlayerPrompt(availablePlay: TPlays[]): TPlays | null;
   getCurrentPlay(): TPlays | null;
   getCurrentPoints(): number;
   incrementCurrentPoints(): void;
