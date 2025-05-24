2abd70acc8acbc039b7e8bb1fd24deb566ea0bc1
Devnielote <danielfc_26@hotmail.com>
2025-05-24 13:24:59 -0600

    wip: ClassicGameSession mock for testing in progress | create UserInterace file

--- src/core/__tests__/ClassicGameSession.test.ts
+++ src/core/__tests__/ClassicGameSession.test.ts
@@ -1,25 +1,9 @@
-import { describe, it, expect, beforeEach } from "vitest";
-import { HumanPlayer } from "../HumanPlayer";
-import { ClassicPlays } from "../types/types";
-import { ClassicRuleset } from "../ClassicRuleset";
-import { ClassicMatch } from "../ClassicMatch";
+import { describe, it, expect, vi } from 'vitest';
 import { ClassicGameSession } from "../ClassicGameSession";
-import { CpuPlayer } from "../CpuPlayer";
 
-describe("ClassicGameSession", () => {
-  let player: HumanPlayer<ClassicPlays>;
-  let cpu: CpuPlayer<ClassicPlays>;
-  let classicRuleset: ClassicRuleset;
-  let gameMatch: ClassicMatch;
-  let classicGameSession: ClassicGameSession;
-
-  beforeEach(() => {
-  player = new HumanPlayer<ClassicPlays>();
-  cpu = new CpuPlayer<ClassicPlays>();
-  classicRuleset = new ClassicRuleset();
-  gameMatch = new ClassicMatch();
-  classicGameSession = new ClassicGameSession();
-
- 
-  })
-})
+const createMockMatch = () => {
+  setRoundsToWin: vi.fn(),
+  getIsGameOver: vi.fn().mockReturnValueOnce(false).mockReturnValueOnce(true),
+  checkIfSessionIsOver: vi.fn(),
+  getAvailablePlays: vi.fn().mockReturnValue(['rock','paper','scissors']),
+});
