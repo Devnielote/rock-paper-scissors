7be3e2a77a8f52b2742cfbe2f997a00809693b0e
Devnielote <danielfc_26@hotmail.com>
2025-06-23 22:24:35 -0600

    feat: implement method for render of the rules button

--- src/core/interfaces/UserInterface.ts
+++ src/core/interfaces/UserInterface.ts
@@ -9,10 +9,12 @@ export interface UserInterface<TPlay = ClassicPlays | BigBangPlays> {
   updatePlayerScore(playerPoints: number): void;
   getUserPlay(): Promise<TPlay>;
   getCpuPlay(): Promise<TPlay>;
+  renderRulesButton(): void;
   renderUserPlay(play: TPlay): void;
   renderCpuPlay(play: TPlay): void;
   renderRoundWinner(result: Player | Cpu | null): void;
   renderPlayAgainButton(onClick: () => void):void;
+  renderRulesButton(onClick: () => void): void;
   cleanWinnersContainer(): void;
   cleanAfterPlaySelect():void;
 }
